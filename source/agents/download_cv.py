3dde8f5fbf439576417bbbd6595356e963c297c9
rushil-misra <rushilmisra@gmail.com>
2025-06-17 17:20:12 +0530

    update

--- source/agents/download_cv.py
+++ source/agents/download_cv.py
@@ -1,20 +1,8 @@
 import os
-from docx import Document
-import pdfplumber
-from langchain_google_genai import ChatGoogleGenerativeAI
-from dotenv import load_dotenv
-from pydantic import BaseModel, Field
 from typing import List, Dict
-from datetime import datetime,timedelta
 from langgraph.types import interrupt, Command
-from langgraph.graph import StateGraph, END
-from agents.functions import extract_resume, download_cv
-from agents.functions2 import extract_qualifications,shortlisting
-from agents.functions3 import connect_sheet,push_data_to_sheet
-from agents.functions4 import email_shortlist
 from typing import TypedDict, Annotated,List,Dict
 from langgraph.graph import add_messages, StateGraph, END
-from langchain_core.messages import AIMessage, HumanMessage
 import os
 
 folder_id = "1kDjNAWM4bE_QKpAGd_FPzJY3bhTGPctF"
---
+++ source/agents/extract_jd.py
@@ -0,0 +1,92 @@
+import os
+from docx import Document
+import pdfplumber
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field
+from typing import List, Dict
+import chromadb
+
+load_dotenv()
+
+
+
+import warnings
+warnings.filterwarnings("ignore", message="CropBox missing from /Page")
+
+client = chromadb.PersistentClient(path="../Databases/Candidate_Database")
+JD_db = client.get_or_create_collection(name="Job_Descriptions")
+
+
+
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash",google_api_key=os.getenv("GEMINI_API_KEY"))
+
+
+
+def extract_text_from_pdf(file_path):
+    with pdfplumber.open(file_path) as pdf:
+        return "\n".join(page.extract_text() or "" for page in pdf.pages)
+    
+
+class Qualifications(BaseModel):
+    """Required qualifications for the job role"""
+    Job_Role : str = Field(description='name of the Job Role')
+    locations: List[str] = Field(..., description="Locations of offices for work")
+    required_experience: str = Field(..., description="Minimum number of experience required in years")
+    Domain_relevance: List[str] = Field(..., description="Relevant Domains for job")
+    Domain_KRA: List[str] = Field(..., description="Key Skills required in that Area")
+
+
+qualify_llm = llm.with_structured_output(Qualifications)
+
+
+def modify_qual(response):
+    review = input(f"current qualifications : \n {response} \n would you like to suggest changes ? (yes/no)")
+    if review.lower() == "no":
+        return False,None
+    else:
+        feedback = input('tell what changes should be made to the qualifications')
+        return True,feedback
+
+
+def extract_qualifications(file_path):
+    try:
+        if file_path.endswith(".pdf"):
+            content =  extract_text_from_pdf(file_path)
+    except:
+        return "file is corrupt"
+    
+    response = qualify_llm.invoke(f''' You have to go through the job descriptions and find relevant job qualifications :
+                       Job_Role = Name of the job role
+                       Locations = [location1,location2,...]
+                       required_experience = 5 (minimum years of experience)
+                       Domain_relevance = [Domain1,Domain2,...]
+                       Domain_KRA = [skill1,skill2,...]
+
+                       Job Description = 
+                       {content}
+                       ''')
+    review, feedback = modify_qual(response)
+    if review:
+        res = qualify_llm.invoke(f'''
+                                modify the previous job qualifications with changes suggested by the user 
+                                previous qualifications = 
+                                {response}
+
+                                suggested modifications = 
+                                
+                                {feedback}
+
+
+                                ''')
+    else:
+        res = response
+    res = res.model_dump()
+
+    JD_db.add(
+        id = [res.Job_role],
+        Documents = [res]
+    )
+
+    
