c67f4edce093878d2f7e5f43558120b3ccb0b537
rushil-misra <rushilmisra@gmail.com>
2025-06-18 16:17:29 +0530

    update

--- source/graph2.py
+++ source/graph2.py
@@ -1,12 +1,16 @@
 from agents.download_cv import check_CVs
 from agents.extract_cv import extract_resume
 from agents.extract_jd import choose_jd
+from subgraph_interview import interview_subgraph, interview_config
 import chromadb
 from langgraph.graph import StateGraph, END
 from typing import TypedDict, Annotated,List,Dict
 from langgraph.graph import add_messages, StateGraph, END
-from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash",google_api_key=os.getenv("GEMINI_API_KEY"))
 
 
 client = chromadb.PersistentClient(path="../Databases/Candidate_Database")
@@ -19,6 +23,7 @@ class JobState(TypedDict):
     logs : Annotated[List, add_messages]
     Job_Description : Dict
     candidates: List[Dict]
+    Interview: Dict
 
 
 def node_check_cvs(state: JobState):
@@ -38,17 +43,34 @@ def node_parse_resumes(state: JobState):
     state['candidates'] = candidates
     return state
 
+
+def node_interview(state: JobState):
+    sub_input = {
+        "jd": state["Job_Description"],
+        "resume": state["candidates"][0],
+        "messages": [],
+    }
+    sub_output = interview_subgraph.invoke(sub_input,config=interview_config)
+    state["Interview"] = sub_output
+    return state
+
+
 builder = StateGraph(JobState)
 
 builder.add_node("Check_CVs", node_check_cvs)
 builder.add_node("Choose_JD", node_choose_jd)
 builder.add_node("Parse_Resumes", node_parse_resumes)
+builder.add_node("interview",node_interview)
+
 
 builder.set_entry_point("Check_CVs")
 
 builder.add_edge("Check_CVs", "Choose_JD")
 builder.add_edge("Choose_JD", "Parse_Resumes")
-builder.add_edge("Parse_Resumes", END)
+builder.add_edge("Parse_Resumes", "interview")
+
+
+
 
 graph = builder.compile()
 
---
+++ source/subgraph_interview.py
@@ -0,0 +1,107 @@
+
+from langgraph.graph import StateGraph, END
+from typing import TypedDict, Annotated,List,Dict
+from langgraph.graph import add_messages, StateGraph, END
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.checkpoint.memory import MemorySaver
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash",google_api_key=os.getenv("GEMINI_API_KEY"))
+
+memory = MemorySaver()
+
+
+class interviewState(TypedDict):
+    jd : Dict
+    resume : Dict
+    messages : Annotated[List, add_messages]
+    decision : Dict
+
+
+def agent_employer(state : interviewState):
+    state['messages'] = (llm.invoke([SystemMessage(content=f'''
+You are an employer interviewing a candidate. your goal is to ask job related questions.
+Job Qualifications :
+{state['jd']}
+
+Ask suitable questions related to Job Description only
+''')])).content
+    return state
+
+def agent_candidate(state : interviewState):
+    state['messages'] = (llm.invoke([SystemMessage(content=f'''
+You are a candidate giving job interview. Answer the question based on your resume.
+Resume :
+{state['resume']}
+''')])).content
+    return state
+
+from pydantic import BaseModel, Field
+
+class InterviewDecision(BaseModel):
+    status: str = Field(..., description="Shortlisted or Rejected")
+    reason: str = Field(..., description="Reason for the decision")
+
+judge_llm = llm.with_structured_output(InterviewDecision)
+
+
+def agent_judge(state: interviewState):
+    messages = state['messages']
+
+    if len(messages) < 3:
+        return "continue"
+
+    judge_check = llm.invoke([
+        SystemMessage(content="Would you like to continue the interview? Respond only with 'Continue' or 'Make a decision'."),
+        *messages
+    ])
+
+    if "continue" in judge_check.content.lower():
+        return "continue"
+
+    structured_response = judge_llm.invoke([
+        SystemMessage(content=f"""
+You are the interviewer. Based on the full conversation, decide whether to Shortlist or Reject the candidate.
+
+Output must follow this structure:
+- status: Shortlisted or Rejected
+- reason: One-line justification
+
+messages : 
+
+{state['messages']}
+""")
+])
+
+    state['decision'] = structured_response.model_dump()
+    return "final"
+
+builder = StateGraph(interviewState)
+
+
+
+
+builder.add_node("employer",agent_employer)
+builder.add_node("candidate",agent_candidate)
+builder.add_node("judge",agent_judge)
+
+builder.add_edge('employer','candidate')
+builder.add_edge('candidate','judge')
+
+builder.set_entry_point('employer')
+
+builder.add_conditional_edges(
+    "judge",
+    agent_judge,
+    {
+        "continue" : "employer",
+        "final" : END
+    }
+)
+
+interview_subgraph = builder.compile(checkpointer=memory)
+
+interview_config = {"configurable": {
+    "thread_id": 1
+}}
